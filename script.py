b8011e8af19ca1199c32e1c78228f9647547a810
REvDi <peresadok33@gmail.com>
2026-04-17 01:10:05 +0300

    feat: Fix thread Race Condition. Add global flag _update_nbu_

--- script.py
+++ script.py
@@ -6,21 +6,28 @@ from config import settings
 import json
 
 
+#Валюты которые нужны, чтоб не брать все курсы из NBU
+USED_CURRENCIES = {"UAH", "USD", "EUR", "CZK", "RUB"}
+
+
 async def nbu_update(redis_client: redis.Redis, http_client: httpx.AsyncClient):
     """парсит валюту банка"""
     try:
+            print("✅PARSER: NBU UPDATE START")
         response = await http_client.get(settings.URL_API_BANK, timeout=5)
         if response.status_code == 200:
             data = response.json()
             rates = {item['cc']: Decimal(str(item['rate'])) for item in data}
             rates["UAH"] = Decimal("1")
-            rates["RUB"] = Decimal("0.42")
-
+            #Хардкодим рубль, потому что нбу банк не отдает курс рубля
+            rates["RUB"] = Decimal("0.57")
+            rates = {k: v for k, v in rates.items() if k in USED_CURRENCIES}
             await redis_client.set(
                 settings.CACHE_KEY,
                 json.dumps({k: str(v) for k, v in rates.items()}),
                 ex=settings.CACHE_TTL,
             )
+            print("⭕PARSER: NBU UPDATE FINISHED")
             return rates
 
     except Exception as e:
@@ -28,9 +35,15 @@ async def nbu_update(redis_client: redis.Redis, http_client: httpx.AsyncClient):
 
 
 
+#Создаем глобальный флаг, чтоб если кэша нет, то 3 одновременных запроса не делали 3 http запроса к банку, проще говоря
+#Проблема Race Condition
+_update_nbu_ = None
+
 
 async def get_nbu_rates(redis_client: redis.Redis, http_client: httpx.AsyncClient):
+    global _update_nbu_
     try:
+        print("GET_NBU_RATES CALLED")
         cache = await redis_client.get(settings.CACHE_KEY)
         if cache:
             data = json.loads(cache)
@@ -38,10 +51,12 @@ async def get_nbu_rates(redis_client: redis.Redis, http_client: httpx.AsyncClien
     except Exception as e:
         print(f"Redis error: {e}")
 
-    asyncio.create_task(nbu_update(redis_client, http_client))
+    if _update_nbu_ is None or _update_nbu_.done():
+        _update_nbu_ = asyncio.create_task(nbu_update(redis_client, http_client))
+    await _update_nbu_
     return {
         "UAH": Decimal("1.0"),
         "USD": Decimal("41.2"),
         "EUR": Decimal("44.5"),
-        "RUB": Decimal("1.7")
+        "RUB": Decimal("0.57")
     }
