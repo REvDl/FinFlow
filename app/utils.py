082da94169a6a99908deeda1f2869f1e860b0219
REvDi <peresadok33@gmail.com>
2026-04-18 21:21:27 +0300

    feat(backend): refresh token is also added to cookies

--- app/utils.py
+++ app/utils.py
@@ -8,7 +8,17 @@ def set_auth_cookies(response: Response, tokens:dict):
         value=tokens["access_token"],
         max_age= (settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60),
         httponly=True,
-        samesite="none",
-        secure=True,
+        samesite="lax",
+        secure=False,
         path="/"
     )
+    if "refresh_token" in tokens:
+        response.set_cookie(
+            key="refresh_token",
+            value=tokens["refresh_token"],
+            max_age=settings.REFRESH_TOKEN_EXPIRE_MINUTES * 60,
+            httponly=True,
+            samesite="lax",
+            secure=False,
+            path="/"
+        )
